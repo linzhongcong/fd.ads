31589fd28ff42532fb4084e5f4bef53eefa1d7bc
alincong <1820716762@qq.com>
2021-11-01 17:22:40 +0800

    更新

--- pages/property/promotion-data/components/tiktok-list-account-synchronization.vue
+++ pages/property/promotion-data/components/tiktok-list-account-synchronization.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <a-popover placement="topLeft">
+    <a-popover placement="topRight">
       <template slot="content">
-        <p>进行广告账号设置，用</p>
-        <p>于获取账户的计划数据</p>
+        <p class="title">进行广告账号设置，用</p>
+        <p class="title">于获取账户的计划数据</p>
       </template>
       <a-button type="primary"
                 class="account-synchronization"
-                @click="openMode('accountSynchronization')">账号同步
+                @click="openMode('accountSynchronization')">
+        账户同步
         <a-icon type="question-circle" />
       </a-button>
     </a-popover>
@@ -15,80 +16,61 @@
     <a-modal title="账号同步"
              v-model="accountSynchronization"
              width="70%">
-      <a-form-model :model="searchForm"
-                    ref="accountSynchronization"
-                    label-align="left"
-                    layout="inline"
-                    :label-col="{ span: 4 }"
-                    :wrapper-col="{ span: 20 }">
-        <a-row :gutter="20">
-          <a-col :xl="{ span: 6}"
-                 :xxl="{ span: 5 }">
-            <a-form-model-item label="账户"
-                               style="width: 100%">
-              <a-select v-model.trim="searchForm.advertiserId"
-                        show-search
-                        allow-clear
-                        option-label-prop="label"
-                        @search="(value) => productionFuzzySearch(value, 'accountList')"
-                        @change="(val) => onSelectChange(val, 'searchForm', 'advertiserId')"
-                        placeholder="账户ID/账号名称">
-                <a-spin v-if="fetching"
-                        slot="notFoundContent"
-                        size="small" />
-                <a-select-option v-for="item in accountList"
-                                 :value="item.employee"
-                                 :key="item.advertiserId"
-                                 :label="item.advertiserName">
-                  {{ item.employee }}
-                </a-select-option>
-                </a-select-option>
-              </a-select>
-            </a-form-model-item>
-          </a-col>
-          <a-col :xl="{ span: 12}"
-                 :xxl="{ span: 10 }">
-            <a-form-model-item label="当前负责人"
-                               style="width: 100%">
-              <SelectUsers v-model.trim="searchForm.realName"
-                           @change="(val) => onSelectChange(val, 'searchForm', 'realName')" />
-            </a-form-model-item>
-          </a-col>
-          <a-col :xl="{ span: 3}"
-                 :xxl="{ span: 5 }">
-            <a-button type="primary"
-                      class="query"
-                      @click="getSyncAccountList('init')">查询</a-button>
-          </a-col>
-          <a-col :xl="{ span: 3}"
-                 :xxl="{ span: 4 }">
-            <a-button type="primary"
-                      class="newAdd"
-                      icon="plus"
-                      @click="openMode('newlyBuild', 'add')">
-              新建
-            </a-button>
-          </a-col>
-        </a-row>
-        <!-- 表格 -->
-        <a-table :columns="accountTable"
-                 :data-source="accountTableData"
-                 :row-key="(record) => record.id"
-                 :loading="loadingTable"
-                 :scroll="{ y: 520 }"
-                 size="small"
-                 :pagination="pagination"
-                 @change="changePage"
-                 class="mt"
-                 bordered>
-          <template slot="operate"
-                    slot-scope="text, record, index"
-                    style="display: flex">
-            <a style="color: red"
-               @click="deletePromotionItem(record)">删除</a>
-          </template>
-        </a-table>
-      </a-form-model>
+      <a-row>
+        <a-col :span="21">
+          <span class="fw">账户：</span>
+          <a-select v-model.trim="searchForm.advertiserName"
+                    show-search
+                    allow-clear
+                    option-label-prop="label"
+                    @search="(value) => productionFuzzySearch(value, 'accountList')"
+                    @change="(val) => onSelectChange(val, 'searchForm', 'accountList')"
+                    placeholder="计划ID/计划名称"
+                    style="width: 30%">
+            <a-spin v-if="fetching"
+                    slot="notFoundContent"
+                    size="small" />
+            <a-select-option v-for="item in accountList"
+                             :value="item.advertiserName"
+                             :key="item.advertiserId"
+                             :label="item.advertiserName">
+              {{ item.employee }}
+            </a-select-option>
+          </a-select>
+          <span class="ml fw">当前负责人：</span>
+          <SelectUsers v-model.trim="searchForm.realName"
+                       @change="(val) => onSelectChange(val, 'searchForm', 'accountList')"
+                       style="width: 25%" />
+          <a-button type="primary"
+                    class="query"
+                    @click="getSyncAccountList('init')">查询</a-button>
+        </a-col>
+        <a-col :span="3">
+          <a-button type="primary"
+                    class="newAdd"
+                    icon="plus"
+                    @click="openMode('newlyBuild', 'add')">
+            新建
+          </a-button>
+        </a-col>
+      </a-row>
+      <a-table :columns="accountTable"
+               :data-source="accountTableData"
+               :row-key="(record) => record.id"
+               :loading="loadingTable"
+               :scroll="{ y: 520 }"
+               size="small"
+               :pagination="pagination"
+               @change="changePage"
+               class="mt"
+               bordered>
+        <template slot="operate"
+                  slot-scope="text, record, index"
+                  style="display: flex">
+          <a style="color: red"
+             @click="deletePromotionItem(record)">删除</a>
+        </template>
+      </a-table>
       <!-- 新建 -->
       <a-modal v-model="newlyBuild"
                title="新建"
@@ -98,24 +80,23 @@
                       :model="newlyBuildForm"
                       :rules="formRules"
                       :label-col="{ span: 4 }"
-                      :wrapper-col="{ span: 12 }">
-
+                      :wrapper-col="{ span: 13 }">
           <a-form-model-item label="账户"
-                             prop="advertiserId">
-            <a-select v-model="newlyBuildForm.advertiserId"
+                             prop="advertiserName">
+            <a-select v-model="newlyBuildForm.advertiserName"
                       show-search
                       allow-clear
                       option-label-prop="label"
-                      @search="(value) => formFuzzySearchList(value, 'searchAccountList')"
-                      @change="(val) => addSelectChange(val, 'newlyBuildForm', 'advertiserName')"
+                      @search="(value) => productionFuzzySearch(value, 'searchAccountList')"
+                      @change="(val) => onSelectChange(val, 'newlyBuildForm', 'searchAccountList')"
                       placeholder="账户ID/账号名称">
               <a-spin v-if="fetching"
                       slot="notFoundContent"
                       size="small" />
               <a-select-option v-for="item in searchAccountList"
-                               :value="item.advertiserId"
+                               :value="item.advertiserName"
                                :key="item.advertiserId"
-                               :label="item.advertiserId">
+                               :label="item.advertiserName">
                 {{ item.employee }}
               </a-select-option>
             </a-select>
@@ -128,7 +109,9 @@
           <a-button key="submit"
                     type="primary"
                     :loading="btnLoading"
-                    @click="submit('newlyBuild')">提交</a-button>
+                    @click="submit('newlyBuild')">
+            提交
+          </a-button>
         </template>
       </a-modal>
       <!-- 自定义页脚 - 账号同步 -->
@@ -146,7 +129,6 @@ import { accountTable } from './js/columns'
 export default {
   data () {
     this.productionFuzzySearch = debounce(this.productionFuzzySearch, 500)
-    this.formFuzzySearchList = debounce(this.formFuzzySearchList, 500)
     return {
       accountSynchronization: false,
       searchForm: {},
@@ -173,21 +155,19 @@ export default {
       searchAccountList: [],
       newlyBuildForm: {},
       formRules: {
-        account: [{ required: true, message: '请选择账户ID/账户名称', trigger: 'change' }],
+        advertiserName: [{ required: true, message: '请选择账户ID/账户名称', trigger: 'change' }],
       },
     }
   },
-  created () {
-    this.getSyncAccountList()
-  },
   methods: {
-    getSyncAccountList (type = "") {
+    getSyncAccountList (type = '') {
       this.loadingTable = true
       let params = JSON.parse(JSON.stringify(this.searchForm))
       params.pageIndex = this.pagination.current
       params.pageSize = this.pagination.pageSize
+      if (!params.advertiserName) delete params.advertiserId
       if (type) {
-        params.pageIndex = this.pagination.current = 1;
+        params.pageIndex = this.pagination.current = 1
       }
       this.$API.syncAccount(params).then(({ code, data }) => {
         this.loadingTable = false
@@ -202,35 +182,14 @@ export default {
     productionFuzzySearch (advertiser, array) {
       if (advertiser === '' || !advertiser.trim()) return (this[array] = [])
       this.fetching = true
-      this.$API
-        .searchAccount(advertiser)
+      const methodName = array === 'accountList' ? 'searchAccount' : 'getReportAdvertiserSearch'
+      this.$API[methodName](advertiser)
         .then(({ code, data }) => {
-
           this.fetching = false
           if (code === 0) {
-            this.accountList = data.map((item) => ({
+            this[array] = data.map((item) => ({
               ...item,
-              employee: `${item.advertiserId} / ${item.advertiserName}`
-            }))
-            console.log(this.accountList);
-          }
-        })
-        .catch(() => (this.fetching = false))
-    },
-
-    // 负责人模糊搜索
-    formFuzzySearchList (advertiser, array) {
-      if (advertiser === '' || !advertiser.trim()) return (this[array] = [])
-      this.fetching = true
-      this.$API
-        .getReportAdvertiserSearch(advertiser.trim())
-        .then(({ code, data }) => {
-
-          this.fetching = false
-          if (code === 0) {
-            this.searchAccountList = data.map((item) => ({
-              ...item,
-              employee: `${item.advertiserId} / ${item.advertiserName}`
+              employee: `${item.advertiserId} / ${item.advertiserName}`,
             }))
           }
         })
@@ -238,27 +197,27 @@ export default {
     },
 
     // 下拉选中
-    onSelectChange (value, obj, field) {
-      this.accountList.map((item) => {
-        if (item.employee === value) this[obj][field] = item[field]
-      })
-    },
-    addSelectChange (value, obj, field) {
-      this.searchAccountList.map((item) => {
-        if (item.advertiserId === value) this[obj][field] = item[field]
+    onSelectChange (value, obj, arr) {
+      this[arr].map((item) => {
+        if (item.advertiserName === value) this[obj].advertiserId = item.advertiserId
       })
     },
 
     // 弹窗
     openMode (prop, type) {
+      this[prop] = true
       this.$refs[prop] && this.$refs[prop].resetFields()
       this.searchForm = {}
-      this[prop] = true
+      this.accountList = []
+      this.newlyBuildForm = {}
+      this.searchAccountList = []
+      console.log(this.newlyBuildForm)
+      if (prop === 'accountSynchronization') {
+        this.getSyncAccountList()
+      }
     },
-
     // 关闭
     closeMode (prop) {
-      this.searchForm = {}
       this[prop] = false
     },
 
@@ -268,18 +227,21 @@ export default {
         if (!valid) return
         this.btnLoading = true
         let params = JSON.parse(JSON.stringify(this.newlyBuildForm))
-        this.$API.postAccount(params).then(({ code, data, msg }) => {
-          if (code === 0) {
-            this.$message.success('新建成功', 1.5)
+        console.log(params)
+        this.$API
+          .postAccount(params)
+          .then(({ code, data, msg }) => {
+            if (code === 0) {
+              this.$message.success('新建成功', 1.5)
+              this.btnLoading = false
+              this[prop] = false
+              this.getSyncAccountList()
+              return
+            }
+            this.$message.error(msg)
             this.btnLoading = false
             this[prop] = false
-            this.getSyncAccountList()
-            return
-          }
-          this.$message.error(msg)
-          this.btnLoading = false
-          this[prop] = false
-        })
+          })
           .catch(() => {
             this[prop] = false
             this.btnLoading = false
@@ -313,12 +275,16 @@ export default {
           })
         },
       })
-    }
-  }
+    },
+  },
 }
 </script>
 
 <style lang="less" scoped>
+.title {
+  font-size: 12px;
+  color: #666;
+}
 .account-synchronization {
   position: absolute;
   z-index: 999;
@@ -326,7 +292,7 @@ export default {
 }
 .query {
   margin-top: 4px;
-  margin-left: -30px;
+  margin-left: 10px;
 }
 .newAdd {
   margin-top: 4px;
@@ -335,5 +301,13 @@ export default {
 .mt {
   margin-top: 20px;
 }
+.footer {
+  margin-right: 8px;
+}
+.ml {
+  margin-left: 15px;
+}
+.fw {
+  font-weight: bold;
+}
 </style>
-
